7a4d7e69e43f280c76883c2dc85675f359919a68
Malik Elate <Malik.e1955@gmail.com>
2020-11-12 21:55:36 -0600

    able to select which and change color

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,15 +1,20 @@
 console.log('hello from client'); 
+// global variables
+let selectedOperator = ''; 
 
 $(document).ready(readyNow); 
 
 function readyNow() { 
-    console.log('hello from JQuery')
-    $('#clear-button').on('click', clearInputs)
-    $('#submit-button').on('click', submit)
-
+    console.log('hello from JQuery');
+    // button click events 
+    $('#clear-button').on('click', clearInputs);
+    $('#submit-button').on('click', submit);
+    // operator click events 
+    $('#add-button').on('click', () => setOperator('add'));
+    $('#subtract-button').on('click', () => setOperator('subtract'))
+    $('#multiply-button').on('click', () => setOperator('multiply'))
+    $('#divide-button').on('click', () => setOperator('divide'))
 } 
-
-// function to clear the inputs of the calculator 
 
 // submit sends the data from the inputs to server
 function submit() { 
@@ -22,15 +27,84 @@ function submit() {
         url: '/numbers', 
         data: { 
             firstNumber: first, 
-            secondNumber: second 
+            secondNumber: second, 
+            operator: selectedOperator
         }
     }).then( function(response) {
-        console.log('recied from server POST:', response)
+        console.log('received from server POST:', response)
         // clear inputs 
         clearInputs(); 
     })
-}
-function clearInputs() { 
-    console.log('clear input was clicked') 
-    $('.input').val('');
 } 
+
+function setOperator(num) { 
+    if (num === 'add') { 
+        console.log('+ selected') 
+        // update selectedOperator 
+        selectedOperator = 'plus'; 
+        // highlight selected 
+        $('#add-button').addClass('red') 
+        // un-highlight all other buttons
+        removeAllElse(selectedOperator); 
+    } 
+    else if(num === 'add') {
+        console.log('+ selected') 
+        $('#add-button').addClass('red') 
+        selectedOperator = 'add'
+        removeAllElse(selectedOperator);
+    } 
+    else if(num === 'subtract') {
+        console.log('- selected') 
+        $('#subtract-button').addClass('red') 
+        selectedOperator = 'subtract'
+        removeAllElse(selectedOperator);
+    } 
+    else if (num === 'multiply') {
+        console.log('* selected') 
+        $('#multiply-button').addClass('red') 
+        selectedOperator = 'multiply'
+        removeAllElse(selectedOperator);
+    } 
+    else if(num === 'divide') {
+        console.log('/ selected') 
+        $('#divide-button').addClass('red') 
+        selectedOperator = 'divide'
+        removeAllElse(selectedOperator);
+    }
+}
+
+// function to clear the inputs of the calculator 
+function clearInputs() { 
+    console.log('inputs cleared') 
+    // resets inputs
+    $('.input').val(''); 
+    // reset selection 
+    $('.operator-button').removeClass('red'); 
+    selectedOperator = ''; 
+}  
+
+function removeAllElse(item) { 
+    if (item === 'plus' )  {
+        $('#subtract-button').removeClass('red');
+        $('#multiply-button').removeClass('red');
+        $('#divide-button').removeClass('red');
+    }
+    else if (item === 'subtract' )  {
+        $('#add-button').removeClass('red');
+        $('#multiply-button').removeClass('red');
+        $('#divide-button').removeClass('red');
+    }
+    else if (item === 'multiply' )  {
+        $('#subtract-button').removeClass('red');
+        $('#add-button').removeClass('red');
+        $('#divide-button').removeClass('red');
+    }
+    else if (item === 'divide' )  {
+        $('#subtract-button').removeClass('red');
+        $('#multiply-button').removeClass('red');
+        $('#add-button').removeClass('red');
+    } 
+    else { 
+        console.log('error deselecting colors')
+    }
+}
